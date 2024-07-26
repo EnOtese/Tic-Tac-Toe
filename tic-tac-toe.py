8b6582bae9bf0e1c538ef705505864b2c21d896e
EnOte <164638951+EnOtese@users.noreply.github.com>
2024-07-26 11:11:02 +0400

    tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -50,7 +50,7 @@ for turn in range(1, 10):
         print('Ходят нолики')
     else:
         turn_char = 'X'
-        print('Ходят крестки')
+        print('Ходят крестики')
     row = int(input("Введите номер строки (1,2,3) ")) - 1
     column = int(input("Введите номер столбца (1,2,3) ")) - 1
     if screen[row][column] == "*":
